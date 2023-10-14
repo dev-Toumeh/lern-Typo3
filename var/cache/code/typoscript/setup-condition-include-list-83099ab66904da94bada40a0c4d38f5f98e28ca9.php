24e991dc5e36dd4475e9300f35c95e6a00ba2cd0
Naseem Toumeh <dev.toumeh@gmail.com>
2023-10-14 15:07:37 +0200

    add basic Extension and link it to the backend

--- var/cache/code/typoscript/setup-condition-include-list-83099ab66904da94bada40a0c4d38f5f98e28ca9.php
+++
@@ -1,3 +0,0 @@
-<?php
-return unserialize('O:61:"TYPO3\\CMS\\Core\\TypoScript\\IncludeTree\\IncludeNode\\RootInclude":2:{s:8:"children";a:0:{}s:10:"lineStream";N;}');
-#
--- var/cache/code/typoscript/sys-template-constants-dbffaf71b2be282e.php
+++
@@ -1,3 +0,0 @@
-<?php
-return unserialize('O:68:"TYPO3\\CMS\\Core\\TypoScript\\IncludeTree\\IncludeNode\\SysTemplateInclude":2:{s:8:"children";a:0:{}s:10:"lineStream";O:51:"TYPO3\\CMS\\Core\\TypoScript\\Tokenizer\\Line\\LineStream":1:{s:5:"lines";a:0:{}}}');
-#
